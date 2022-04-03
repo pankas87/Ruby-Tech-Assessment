051d39f426eb7dc72dfee2cbae8053a57571bbfa
Leonardo Graterol <pankas87@gmail.com>
2022-04-03 16:15:22 -0400

    Some changes

--- digits.rb
+++ digits.rb
@@ -1,19 +1,17 @@
 # digits.rb
 
 module Digits
+  # Expected Behavior:
+  # 16 --> 1 + 6 = 7
+  # 942 --> 9 + 4 + 2 = 15 --> = 1 + 5 = 6
+  # 132189 --> 1 + 3 + 2 + 1 + 8 + 9 = 24 --> 2 + 4 = 6
+  # 493193 --> 4 + 9 + 3 + 1 + 9 + 3 = 29 --> 2 + 9 = 11 --> 1 + 1 = 2
   def Digits.add(input)
     sum = 0
 
     input = Float(input) if input.class == String
     input = input.to_i if input.class != Fixnum
     input = input * -1 if input < 0
-
-    # 16 --> 1 + 6 = 7
-    # 942 --> 9 + 4 + 2 = 15 --> = 1 + 5 = 6
-    # 132189 --> 1 + 3 + 2 + 1 + 8 + 9 = 24 --> 2 + 4 = 6
-    # 493193 --> 4 + 9 + 3 + 1 + 9 + 3 = 29 --> 2 + 9 = 11 --> 1 + 1 = 2
-
-
     sum = input
 
     while sum >= 10
--- spec/digits_spec.rb
+++ spec/digits_spec.rb
@@ -1,7 +1,12 @@
 # digits_spec.rb
 require 'bigdecimal'
-require '../digits'
+require_relative '../digits'
 
+# Expected Behavior:
+# 16 --> 1 + 6 = 7
+# 942 --> 9 + 4 + 2 = 15 --> = 1 + 5 = 6
+# 132189 --> 1 + 3 + 2 + 1 + 8 + 9 = 24 --> 2 + 4 = 6
+# 493193 --> 4 + 9 + 3 + 1 + 9 + 3 = 29 --> 2 + 9 = 11 --> 1 + 1 = 2
 RSpec.describe("#add") do
   context "integers" do
     it "correctly adds digits" do
